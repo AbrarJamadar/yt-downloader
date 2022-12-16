7fa5b8563e5c73a20dfd1c162f097389e32dfdb7
Abrar Jamadar <71552453+AbrarJamadar@users.noreply.github.com>
2022-12-16 20:56:37 +0530

    .

--- ytdownloader.py
+++ ytdownloader.py
@@ -3,7 +3,7 @@ from pytube import YouTube
 
 def Download(link):
     yt_obj = YouTube(link)
-    # yt_obj = yt_obj.streams.get_by_resolution("720p", "144p")
+    # yt_obj = yt_obj.streams.get_by_resolution("720p")
     yt_obj = yt_obj.streams.get_highest_resolution()
     try:
         yt_obj.download()
